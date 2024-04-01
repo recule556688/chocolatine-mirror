53750b179a812713f9fa74cf35abd3d8b9ab06c9
Grange Luca <ledombre@gmail.com>
2024-04-01 06:45:24 +0200

    Add chocolatine-mirror.c file

---
+++ chocolatine-mirror.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2024
+** undefined
+** File description:
+** chocolatine-mirror
+*/
+#include <unistd.h>
+
+int main()
+{
+    write(1, "Hello World !", 13);
+    return 0;
+}
